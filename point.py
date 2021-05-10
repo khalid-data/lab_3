5835a526c69b2fc83dfe25eeadbb759a557a250a
khali <khalid.tafla@campus.technion.ac.il>
2021-05-10 23:58:44 +0300

    Initial commit

---
+++ point.py
@@ -0,0 +1,16 @@
+class Point:
+    def __init__(self, name, coordinates=None):
+        self.name = name
+        self.coordinates = []
+        if coordinates:
+            self.set_coordinates(coordinates)
+
+    def distance_to(self, coordinates):
+        if not self.coordinates:
+            print('Point', self.name, 'not initiated. Please provide coordinates in init or call set_coordinates')
+            return 0
+        # return sum([(my-his)**2 for my, his in zip(self.coordinates, coordinates)])**0.5
+        return sum([abs(my-his) for my, his in zip(self.coordinates, coordinates)]);
+
+    def set_coordinates(self, coordinates):
+        self.coordinates = [float(x) for x in coordinates]
